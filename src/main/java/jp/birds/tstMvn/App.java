6eb183806a0f14cc163b0a4e9ce8787dce9f8958
0450m <0450m@192.168.13.131>
2019-01-10 16:02:32 +0900

    １・９

---
+++ src/main/java/jp/birds/tstMvn/App.java
@@ -0,0 +1,13 @@
+package jp.birds.tstMvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
